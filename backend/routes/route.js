8401968141f9f8cef5dfc23c853c34925e701810
Jesron <162868061+jesronstark@users.noreply.github.com>
2026-01-23 22:10:33 +0530

    Update route.js

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,6 +1,19 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 const router = require('express').Router();
 
 // const { adminRegister, adminLogIn, deleteAdmin, getAdminDetail, updateAdmin } = require('../controllers/admin-controller.js');
