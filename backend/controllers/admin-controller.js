fad167ad37b98eceb3fa5f4260769b0142143dd1
Jesron <162868061+jesronstark@users.noreply.github.com>
2026-02-02 18:53:46 +0530

    Update admin-controller.js

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -1,3 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const bcrypt = require('bcrypt');
 const Admin = require('../models/adminSchema.js');
 const Sclass = require('../models/sclassSchema.js');
